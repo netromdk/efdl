55bc9de6f248884a23e7e7f0c2652ee6d46fc2e9
Morten Kristensen <msk@nullpointer.dk>
2014-05-13 23:06:31 +0200

    Initial Cmake setup.

---
+++ cmake/deps.cmake
@@ -0,0 +1,10 @@
+# Qt 5
+SET(CMAKE_AUTOMOC ON) # Moc when necessary.
+
+# As moc files are generated in the binary dir, tell CMake to always
+# look for includes there:
+SET(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+FIND_PACKAGE(Qt5Core REQUIRED)
+FIND_PACKAGE(Qt5Network REQUIRED)
+
---
+++ cmake/mac.cmake
@@ -0,0 +1,11 @@
+SET(SDK_MIN "10.9")
+SET(SDK "10.9")
+SET(DEV_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDK}.sdk")
+
+ADD_DEFINITIONS(
+  -DMAC
+  -DGCC_VISIBILITY
+  -mmacosx-version-min=${SDK_MIN}
+  )
+
+SET(CMAKE_OSX_SYSROOT ${DEV_SDK})
