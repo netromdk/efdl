73184b7cd05f9593ffb8268fd28f33abd84469f1
Morten Kristensen <msk@nullpointer.dk>
2014-05-17 21:52:11 +0200

    Added existence check for Mac OSX SDK directory.

--- cmake/mac.cmake
+++ cmake/mac.cmake
@@ -2,6 +2,12 @@ SET(SDK_MIN "10.8")
 SET(SDK "10.8")
 SET(DEV_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDK}.sdk")
 
+IF (NOT EXISTS "${DEV_SDK}" AND NOT IS_DIRECTORY "${DEV_SDK}")
+  MESSAGE("Could not find Mac OSX SDK at: ${DEV_SDK}")
+  MESSAGE("Aborting!")
+  RETURN()
+ENDIF()
+
 ADD_DEFINITIONS(
   -DMAC
   -DGCC_VISIBILITY
