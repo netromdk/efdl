18d32d9e040f166fd40f6d63ee7f4e64b9e77b19
Morten Kristensen <msk@nullpointer.dk>
2014-05-18 21:37:43 +0200

    makedist: osx: copy all Qt frameworks over and call install_name_tool on efdl and individual frameworks.

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -9,8 +9,24 @@ ZIP_DIST=`echo $3 | sed 's/ /\\\\ /g'`
 
 eval mkdir -p ${DIST_DIR}
 
-# Copy binary
-eval cp ${BIN_DIR}/efdl ${DIST_DIR}
+# Copy binary.
+BINARY_IN=${BIN_DIR}/efdl
+eval cp ${BINARY_IN} ${DIST_DIR}
+BINARY_OUT=${DIST_DIR}/efdl
+
+# Copy Qt libraries.
+if [ "`uname`" = "Darwin" ]; then
+    for frmw in `otool -L ${BINARY_OUT} | grep -ie "qt.*\.framework" | xargs -I{} echo "{}" | awk '{print $1}'`; do
+        cp ${frmw} ${DIST_DIR}
+        name=`echo ${frmw} | sed -E 's/(.+)([Qq][Tt].+)(\.[Ff][Rr][Aa][Mm][Ee][Ww][Oo][Rr][Kk].*)/\2/'`
+        install_name_tool -id "@executable_path/${name}" ${DIST_DIR}/${name}
+        install_name_tool -change ${frmw} "@executable_path/${name}" ${BINARY_OUT}
+        if [ "${name}" == "QtNetwork" ]; then
+            core=`otool -L ${DIST_DIR}/${name} | grep -ie "qtcore.*\.framework" | xargs -I{} echo "{}" | awk '{print $1}'`
+            install_name_tool -change ${core} "@executable_path/QtCore" ${DIST_DIR}/${name}
+        fi
+    done
+fi
 
 # Create zip file.
 eval echo "Creating zip file: ${ZIP_DIST}"
