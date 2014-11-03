dfaa1771838fe7e343c9b3749dd9035ce36549d9
Morten Kristensen <msk@nullpointer.dk>
2014-11-03 18:46:50 +0100

    Have OSX SDK fallback to /Developer/SDKs

--- cmake/mac.cmake
+++ cmake/mac.cmake
@@ -1,11 +1,16 @@
 SET(SDK_MIN "10.7")
 SET(SDK "10.7")
 SET(DEV_SDK "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${SDK}.sdk")
+SET(DEV_SDK2 "/Developer/SDKs/MacOSX${SDK}.sdk")
 
 IF (NOT EXISTS "${DEV_SDK}" AND NOT IS_DIRECTORY "${DEV_SDK}")
-  MESSAGE("Could not find Mac OSX SDK at: ${DEV_SDK}")
-  MESSAGE("Aborting!")
-  RETURN()
+  IF (NOT EXISTS "${DEV_SDK2}" AND NOT IS_DIRECTORY "${DEV_SDK2}")
+    MESSAGE("Could not find Mac OSX SDK at: ${DEV_SDK}")
+    MESSAGE("Aborting!")
+    RETURN()
+  ELSE()
+    SET(DEV_SDK ${DEV_SDK2})
+  ENDIF()
 ENDIF()
 
 ADD_DEFINITIONS(
